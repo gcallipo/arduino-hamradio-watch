ffb0df982136fba102028e2870a5b673b730c4bb
gcallipo <ik8yfw@libero.it>
2015-01-12 21:30:18 +0100

    Create preferences.h
    Preference file, you must set the latitude and longitude of you QTH and
    also the keplerian element of two satellite.
    
    You can find new keplerian datas from 
    Norad site:  http://www.celestrak.com/NORAD/elements/
    or 
    Amsat site: http://www.amsat.org/amsat/keps/

---
+++ src/preferences.h
@@ -0,0 +1,33 @@
+/*************************************************
+ *              Preferences
+ *        Put here your preferences 
+ *************************************************/
+
+/*- QTH */
+const double l_qth=40.6454;//latitude
+const double n_qth=14.9469;//longitude  
+
+/*- Orbital Elements PRIMARY SATELLITE - 5.1.2015 */
+// ISS
+const double t1=1.90620712; //epoch
+const double i1=51.6456;// incl
+const double r1=193.9106;// raan
+const double e1=0.0007121;// eccy
+const double w1=197.4499;//argp
+const double u1=315.2103;//ma
+const double m1=15.52915813;//mm
+const double g1=0.00012362;//decy
+const double o1=92216;//revn
+
+/*- Orbital Elements SECONDARY SATELLITE - 5.1.2015 */
+// Oscar 73
+const double t2=1.69133335; //epoch
+const double i2=97.75371;// incl
+const double r2=70.4616;// raan
+const double e2=0.0059726;// eccy
+const double w2=318.9522;//argp
+const double u2=40.7203;//ma
+const double m2=14.79379227;//mm
+const double g2=2.132e-05;//decy
+const double o2=5879;//revn
+
